910c3911498ec2ec7f36f3789dd21ad0e63c0541
yz9527 <1807822529@qq.com>
2025-07-07 21:59:56 +0800

    Changes

---
+++ python_100day/day15/07071.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+a="sdasdas"
+
+if __name__ == '__main__':
+    unittest.main()
+b=12312
+print(a)
